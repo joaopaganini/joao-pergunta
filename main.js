5e736bda9224f9a91250c459087c1028ad8a0fe3
João.paganini <125562491+joaopaganini@users.noreply.github.com>
2024-04-30 11:38:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function verificarTime() {
+  
+    var continuarPerguntando = true;
+  
+    while (continuarPerguntando) {
+      
+      var respostaTime = prompt("Qual foi o ultimo titulo da ferrari na F1??");
+  
+      
+      if (respostaTime.toLowerCase() === "2008") {
+        alert("Isso mesmo! O umtimo titulo foi em 2008!");
+        continuarPerguntando = false; // Sai do loop após a resposta correta
+      } else {
+        alert("Não é isso! Tente novamente.");
+      }
+    }
+  }
+  
+  // Chamada da função para iniciar o processo
+  verificarTime();
+  
+    
+  
